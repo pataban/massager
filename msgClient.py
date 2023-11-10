224fa434aa281b5609e861894f60622fa2e6762c
pataban <brajerpatryk@gmail.com>
2023-11-10 19:09:10 +0100

    upload project version 2

--- msgClient.py
+++ msgClient.py
@@ -7,61 +7,139 @@ class Gui(tk.Frame):
         super().__init__(master)
         master.title("message server client")
         self.pack()
-        self.createWidgets()
+        self.dstUserId=""
+        self.registrationFrame=None
+        self.chatFrame=None
+        self.userListFrame=None
+        self.createRegistrationLayout()
     
-    def createWidgets(self):
-        self.registrationFrame=tk.Frame()
+    def createRegistrationLayout(self):
+        if(self.chatFrame!=None):   
+            self.chatFrame.pack_forget()
+            self.pack()
+        if(self.registrationFrame!=None):
+            self.registrationFrame.pack(side="top") 
+            self.pack()   
+            return
+        self.registrationFrame=tk.Frame(self)
         self.registrationFrame.pack(side="top")
+
+        idLabel=tk.Label(self.registrationFrame,text="User Id:")
+        idLabel.grid(row=0,column=0)
         self.idInput=tk.Entry(self.registrationFrame)
-        #self.idInput.pack(side="top")
-        self.idInput.grid(row=0)
-        self.idInputButton=tk.Button(self.registrationFrame,text="join",command=self.joinChatroom)
-        #self.idInputButton.pack(side="bottom")
-        self.idInputButton.grid(row=1)
-        self.idInputResult=tk.Label(self.registrationFrame,text="")
-        #self.idInputResult.pack(side="right")
-        self.idInputResult.grid(row=3)
+        self.idInput.grid(row=0,column=1)
+        
+        passLabel=tk.Label(self.registrationFrame,text="Password:")
+        passLabel.grid(row=1,column=0)
+        self.passInput=tk.Entry(self.registrationFrame)
+        self.passInput.grid(row=1,column=1)
+
+        self.loginButton=tk.Button(self.registrationFrame,text="login",command=self.loginUser)
+        self.loginButton.grid(row=2,column=0)
+        self.registerButton=tk.Button(self.registrationFrame,text="register",command=self.registerUser)
+        self.registerButton.grid(row=2,column=1)
+        
+        self.loginResult=tk.Label(self.registrationFrame,text="")
+        self.loginResult.grid(row=3,column=0)
         
     def createChatroomLayout(self):
-        self.registrationFrame.pack_forget()
-        self.pack()
+        if(self.registrationFrame!=None):   
+            self.registrationFrame.pack_forget()
+            self.pack()
+        if(self.chatFrame!=None):   
+            self.chatFrame.pack(side="top")
+            self.chatIdLabel["text"]="Chat with: "+self.dstUserId
+            if(self.chatIdLabel["text"][-1]==' '):
+                self.chatIdLabel["text"]+="everyone"
+            self.pack()
+            return
         self.chatFrame=tk.Frame(self)
         self.chatFrame.pack(side="top")
-        self.userIdLabel=tk.Label(self.chatFrame,text="userId: "+self.userId)
-        self.userIdLabel.pack(side="top")
-        self.messagingFrame=tk.Frame(self.chatFrame)
-        self.messagingFrame.pack(side="bottom")
-        self.messageBox=tk.Frame(self.messagingFrame)
-        self.messageBox.pack(side="top")
+
+        self.chatInfoFrame=tk.Frame(self.chatFrame)
+        self.chatInfoFrame.grid(row=0)
+        userIdLabel=tk.Label(self.chatInfoFrame,text="User Id: "+self.userId)
+        userIdLabel.grid(row=0,column=0)
+        self.chatIdLabel=tk.Label(self.chatInfoFrame,text="Chat with: "+self.dstUserId)
+        if(self.chatIdLabel["text"][-1]==' '):
+            self.chatIdLabel["text"]+="everyone"
+        self.chatIdLabel.grid(row=0,column=1)
+        showUsersButton=tk.Button(self.chatInfoFrame,text="Show users",command=self.createUserList)
+        showUsersButton.grid(row=0,column=2)
+        logoutButton=tk.Button(self.chatInfoFrame,text="Logout",command=self.logoutUser)
+        logoutButton.grid(row=0,column=3)
+
+
+        self.messagesFrame=tk.Frame(self.chatFrame)
+        self.messagesFrame.grid(row=1)
         self.messageLabels=[]
         for i in range(1,10):
-            self.messageLabels.append(tk.Label(self.messageBox,text=""))
+            self.messageLabels.append(tk.Label(self.messagesFrame,text=""))
             self.messageLabels[-1].grid(row=i-1,column=0)
+        
+        self.messagingFrame=tk.Frame(self.chatFrame)
+        self.messagingFrame.grid(row=2)
         self.messageEntry=tk.Entry(self.messagingFrame)
-        self.messageEntry.pack(side="bottom")
+        self.messageEntry.grid(row=0,column=0)
         self.messageEntry.bind("<Return>",self.sendMessage)
+        self.messageEntryButton=tk.Button(self.messagingFrame,text="Send",command=self.sendMessage)
+        self.messageEntryButton.grid(row=0,column=1)
+
         self.waitForMessages()
 
 
-    def joinChatroom(self):
-        if(self.idInput.get()==""):
-            self.idInputResult["text"]="no id provided"
+    def registerUser(self):
+        if((self.idInput.get()=="")or(self.passInput.get()=="")):
+            self.loginResult["text"]="no id or password provided"
             self.update()
             return
-        res=req.post("http://127.0.0.1:5000/api/users/register?id="+self.idInput.get())
+        res=req.post("http://127.0.0.1:5000/api/users/register"
+            ,data={"userId":self.idInput.get(),"password":self.passInput.get()})
         if(res.text=="Registered successfully"):
             self.userId=self.idInput.get()
+            self.idInput.delete(0,1000)
+            self.userPass=self.passInput.get()
+            self.passInput.delete(0,1000)
             return self.createChatroomLayout()
         else:
-            self.idInputResult["text"]="Id alredy taken"
+            self.loginResult["text"]="Id alredy taken"
             self.update()
             return
-            """self.userId=self.idInput.get()
-            return self.createChatroomLayout()"""
+            
+
+    def loginUser(self):
+        if((self.idInput.get()=="")or(self.passInput.get()=="")):
+            self.loginResult["text"]="no id or password provided"
+            self.update()
+            return
+        res=req.post("http://127.0.0.1:5000/api/users/login"
+            ,data={"userId":self.idInput.get(),"password":self.passInput.get()})
+        if(res.text=="Login successfull"):
+            self.userId=self.idInput.get()
+            self.idInput.delete(0,1000)
+            self.userPass=self.passInput.get()
+            self.passInput.delete(0,1000)           
+            return self.createChatroomLayout()
+        else:
+            self.loginResult["text"]="wrong id or password"
+            self.update()
+            return
+    
+    def logoutUser(self):
+        res=req.post("http://127.0.0.1:5000/api/users/logout"
+            ,data={"userId":self.userId,"password":self.userPass})
+        print(res.text)
+        if(res.text=="Logout successfull"):
+            return self.createRegistrationLayout()
+        return
+    
 
     def waitForMessages(self):
         while(True):
-            res=req.get("http://127.0.0.1:5000/api/users?id="+self.userId).json()
+            res_raw=req.get("http://127.0.0.1:5000/api/users"
+                ,data={"userId":self.userId,"password":self.userPass})
+            #print(res_raw.text)
+            res=res_raw.json()
             res.reverse()
             res.append(None)
             j=0
@@ -70,15 +148,80 @@ class Gui(tk.Frame):
                 while(i+1<len(self.messageLabels)):
                     self.messageLabels[i]["text"]=self.messageLabels[i+1]["text"]
                     i+=1
-                self.messageLabels[-1]["text"]=res[j]["senderId"]+":\n"+res[j]["msg"]
+                self.messageLabels[-1]["text"]=res[j]["srcId"]+":\n"+res[j]["msg"]
                 j+=1
-            self.messageBox.update()
-            time.sleep(0.5)
+            self.messagesFrame.update()
+            time.sleep(1)
     
-    def sendMessage(self,event):
-        res=req.post("http://127.0.0.1:5000/api/send/all?id="+self.userId,{"msg":self.messageEntry.get()})
+    def sendMessage(self,event=None):
+        if(self.dstUserId==""):
+            res=req.post("http://127.0.0.1:5000/api/send/all"
+                ,{"userId":self.userId,"password":self.userPass,"msg":self.messageEntry.get()})
+        else:
+            res=req.post("http://127.0.0.1:5000/api/send",{"userId":self.userId,
+                "password":self.userPass,"dstUserId":self.dstUserId,"msg":self.messageEntry.get()})
+        i=0
+        while(i+1<len(self.messageLabels)):
+            self.messageLabels[i]["text"]=self.messageLabels[i+1]["text"]
+            i+=1
+        self.messageLabels[-1]["text"]=self.userId+":\n"+self.messageEntry.get()
         self.messageEntry.delete(0,1000)
-        self.messageBox.update()
+        self.messagesFrame.update()
+
+    def createUserList(self):
+        if(self.chatFrame!=None):   
+            self.chatFrame.pack_forget()
+            self.pack()
+        if(self.registrationFrame!=None):   
+            self.registrationFrame.pack_forget()
+            self.pack()
+        if(self.userListFrame!=None):
+            self.userListFrame.pack(side="top") 
+            self.updateUserList()
+            self.pack()   
+            self.userListFrame.update()
+            return
+        self.userListFrame=tk.Frame(self)
+        self.userListFrame.pack(side="top")
+
+        self.selectUserListbox=tk.Listbox(self.userListFrame,width=50,height=15)
+        self.selectUserListbox.pack(side="top")
+        #self.selectUserListbox.grid(row=0)
+        
+        selectUserButton=tk.Button(self.userListFrame,text="Enter",command=self.chatSelected)
+        selectUserButton.pack(side="bottom")
+        #selectUserButton.grid(row=1)
+        return self.updateUserList()
+
+
+    def updateUserList(self):
+        res_raw=req.get("http://127.0.0.1:5000/api/users/all"
+            ,data={"userId":self.userId,"password":self.userPass})
+        #print(res_raw.text)
+        res=res_raw.json()
+        #print(res)
+        self.selectUserListbox.delete(0,tk.END)
+        self.selectUserListbox.insert(1,"everyone ")
+        res.append(None)
+        i=0
+        while(res[i]!=None):
+            text=res[i]["userId"]
+            if(res[i]["active"]==True):
+                text+="\t(active)"
+            else:
+                text+="\t(pasive)"
+            self.selectUserListbox.insert(i+2,text)
+            i+=1
+
+
+    def chatSelected(self):
+        sel=self.selectUserListbox.curselection()
+        if(len(sel)==0):
+            return
+        self.dstUserId=self.selectUserListbox.get(sel)[:-9]
+        self.userListFrame.pack_forget()
+        self.pack()
+        return self.createChatroomLayout()
 
 Gui().mainloop()
 
@@ -88,7 +231,6 @@ Gui().mainloop()
 data={"recieverId":"qwe","msg":"hihi"}
 res=req.post("http://127.0.0.1:5000/api/send?id=asd",data)
 print(res.text)
-
 data2={"msg":"nooooo"}
 res=req.post("http://127.0.0.1:5000/api/send/all?id=asd",data2)
 print(res.text)
--- msgServer.py
+++ msgServer.py
@@ -1,15 +1,53 @@
+
 import flask
 from flask import request, jsonify
+import sqlalchemy as db
+import pandas as pd
+
+engine = db.create_engine('sqlite:///msgServer.db')
+connection = engine.connect()
+metadata = db.MetaData()
+
+users = db.Table('users', metadata,
+              db.Column('id', db.String(255),nullable=False),
+              db.Column('password', db.String(255), nullable=False),
+              db.Column('active', db.Boolean(), default=False)
+              )
+
+messages = db.Table('messages', metadata,
+              db.Column('srcId', db.String(255),nullable=False),
+              db.Column('dstId', db.String(255), nullable=False),
+              db.Column('msg', db.String(255), nullable=False),
+              db.Column('send', db.Boolean(), default=False)
+              )
+
+metadata.create_all(engine)
+
+query=db.select([users]).where(users.columns.id == "qwe")
+resultProxy = connection.execute(query)
+resultSet = resultProxy.fetchall()
+if (len(resultSet)==0):
+    query = db.insert(users).values(id="qwe", password="rty")
+    resultProxy = connection.execute(query)
+
+query=db.select([users]).where(users.columns.id == "asd")
+resultProxy = connection.execute(query)
+resultSet = resultProxy.fetchall()
+if (len(resultSet)==0):
+    query = db.insert(users).values(id="asd", password="fgh")
+    resultProxy = connection.execute(query)
+
+query=db.update(users).values(active = False)
+resultProxy = connection.execute(query)
+
+"""query=db.select([messages])
+resultProxy = connection.execute(query)
+resultSet = resultProxy.fetchall()
+print(resultSet)"""
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
-users = {
-    "qwe":[],
-    "asd":[{"senderId":"qwe","msg":"hello"}],
-    "zxc":[{"senderId":"qwe","msg":"heloo"},{"senderId":"asd","msg":"hi"}]
-}
-
 
 @app.route('/', methods=['GET','post'])
 def home():
@@ -19,60 +57,143 @@ def home():
 
 @app.route('/api/users/all', methods=['GET'])
 def api_users_all():
-    return jsonify(users.keys())
+    connection = engine.connect()
+    query=db.select([users])
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+    res=[]
+    for u in resultSet:
+        res.append({"userId":u[0],"active":u[2]})
+    return jsonify(res)
 
 
-@app.route('/api/users/register', methods=['POST'])      #zrobic post nie get
+@app.route('/api/users/register', methods=['POST'])
 def api_register_user():
-    # Check if an user ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
+    data=request.form
+    id=data["userId"]
+    password=data["password"]
+    if((id=="") or (password=="")):
+        return "Error: No id or password provided. Please specify an id and password."
 
-    # If provided ID is not taken, create new user.
-    # If provided ID is taken, display an error in the browser.
-    if id in users:
-        return "Id not available. Please select new ID"
+    connection = engine.connect()
+    query=db.select([users]).where(users.columns.id == id)
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+    
+    if (len(resultSet)>0):
+        return "Id not available. Please select different ID"
     else:
-        users[id]=[]
+        query = db.insert(users).values(id=id, password=password, active=True)
+        resultProxy = connection.execute(query)
         return "Registered successfully"
+
+
+@app.route('/api/users/login', methods=['POST'])
+def api_login_user():
+    data=request.form
+    id=data["userId"]
+    password=data["password"]
+    if((id=="") or (password=="")):
+        return "Error: No id or password provided. Please specify an id and password."
+
+    connection = engine.connect()
+    query=db.select([users]).where(users.columns.id == id)
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+    
+    if (len(resultSet)==0):
+        return "User does not exist"
+    
+    if((resultSet[0]["id"]!=id) or (resultSet[0]["password"]!=password)):
+        return "wrong login or password"
+    
+    query=db.update(users).values(active = True).where(users.columns.id==id)
+    resultProxy = connection.execute(query)
+    return "Login successfull"
+
+
+def verifyUser(id,password):
+    if((id=="") or (password=="")):
+        return False
+
+    connection = engine.connect()
+    query=db.select([users]).where(users.columns.id == id)
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+    
+    if (len(resultSet)==0):
+        return False
+    
+    if(str((resultSet[0]["id"])==str(id)) and (str(resultSet[0]["password"])==str(password))):
+        return True
+
+
+@app.route('/api/users/logout', methods=['POST'])
+def api_logout_user():
+    data=request.form
+    id=data["userId"]
+    if(not verifyUser(id,data["password"])):
+        return "User not recognised"
+
+    connection = engine.connect()
+    query=db.update(users).values(active = False).where(users.columns.id==id)
+    resultProxy = connection.execute(query)
+    return "Logout successfull"
 
 
 @app.route('/api/users', methods=['GET'])
 def api_user_recieve():
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
+    data=request.form
+    if(not verifyUser(data["userId"], data["password"])):
+        return "User not valid"
 
-    res=users[id]
-    users[id]=[]
+    connection = engine.connect()
+    query=db.select([messages])
+    query=query.where(db.and_(messages.columns.send == False
+        , messages.columns.dstId == data["userId"]))
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+
+    query=db.update(messages).values(send = True).where(db.and_(messages.columns.send == False
+        , messages.columns.dstId == data["userId"]))
+    resultProxy = connection.execute(query)
+
+    print(resultSet)
+    res=[]
+    for msg in resultSet:
+        res.append({"srcId":msg[0],"dstId":msg[1],"msg":msg[2]})
     return jsonify(res)
+
 
 @app.route('/api/send', methods=['POST'])
 def api_user_send():
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
-
     data=request.form
-    users[data["recieverId"]].append({"senderId":id,"msg":data["msg"]})
+    if(not verifyUser(data["userId"], data["password"])):
+        return "User not valid"
+
+    connection = engine.connect()
+    query = db.insert(messages).values(srcId=data["userId"], dstId=data["dstUserId"],msg=data["msg"])
+    resultProxy = connection.execute(query)
+    
     return "message send"
+
 
 @app.route('/api/send/all', methods=['POST'])
 def api_user_send_everone():
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
-
     data=request.form
-    for u in users:
-        users[u].append({"senderId":id,"msg":data["msg"]})
+    if(not verifyUser(data["userId"], data["password"])):
+        return "User not valid"
+
+    connection = engine.connect()
+    query=db.select([users])
+    resultProxy = connection.execute(query)
+    resultSet = resultProxy.fetchall()
+    msgList=[]
+    for u in resultSet:
+        if(u[0]!=data["userId"]):
+            msgList.append({"srcId":data["userId"],"dstId":u[0],"msg":data["msg"]})
+    query = db.insert(messages)
+    resultProxy = connection.execute(query,msgList)
     return "messages send"
 
 
